c1dc6fe8e12bed964184279beff38fedbeedbacf
Riggs Gorman <riggsgorman@Riggss-MacBook-Pro-2.local>
2024-11-07 00:47:14 -0600

    allowed origins fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const port = 3000;
 
-const allowedOrigins = [
+/* const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://calendar-service-8q9b.onrender.com',
@@ -30,7 +30,7 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-};
+}; */
 
 // Middleware
 app.use(cors());
